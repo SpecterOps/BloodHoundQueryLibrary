165439ff8c340ba72400f24906cc55671fb43d61
Joey Dreijer <jdreijer@specterops.io>
2025-06-13 15:48:44 +0200

    Check if a query with duplicate guid exists

--- tests/schema.py
+++ tests/schema.py
@@ -1,16 +1,21 @@
-from pydantic import BaseModel
-from typing import Optional, Union, List
+from pydantic import BaseModel, field_validator
+from typing import Optional, Union
 
 
 class CypherQuery(BaseModel):
     name: str
     guid: str
     prebuilt: bool = False
-    platform: Union[str, List[str]]
+    platform: Union[str, list[str]]
     category: str
     description: Optional[str] = None
     query: str
     revision: int
     note: Optional[str] = None
-    resources: Optional[Union[str, List[str]]] = None
-    acknowledgement: Optional[Union[str, List[str]]] = None
+    resources: Optional[Union[str, list[str]]] = None
+    acknowledgement: Optional[Union[str, list[str]]] = None
+
+    @field_validator('platform', mode='after')  
+    @classmethod
+    def platform_is_list(cls, value: str | list[str]) -> list[str]:
+        return value if isinstance(value, list) else [value]
